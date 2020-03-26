78a013fb93e1a36901c720640dcc4a554f7681ec
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-26 23:31:03 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,10 +14,10 @@ class Pokemon
 
   def self.save(name,type,db)
     sql =<<-SQL
-      INSERT INTO pokemon (name, type) 
+      INSERT INTO pokemon (name, type)
       VALUES (?,?)
     SQL
-    
+
     db.execute(sql, name, type)
   end
 
